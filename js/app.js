a0a2753f7d5bbffb2ddb5dbcf816499960020f45
mrmojica <mrmojica510@gmail.com>
2016-07-21 12:41:52 -0700

    added guidelines to display + user select button

--- js/app.js
+++ js/app.js
@@ -26,33 +26,111 @@
 					//assign variable
 			//summary page
 $(document).ready(function() {
-	var question1 = {
-		question: 'question1',
-		choices: ['answer1','answer2', 'answer3', 'answer4'],
-		correctAnswer: question1.choices[0];
-	}
 
-	var question2 = {
-		question: 'question1',
-		choices: ['answer1','answer2', 'answer3', 'answer4'],
-		correctAnswer: question1.choices[0];
-	}
+// variables**********
 
-	var question3 = {
+var questionList = [
+[
+	 {
 		question: 'question1',
 		choices: ['answer1','answer2', 'answer3', 'answer4'],
-		correctAnswer: question1.choices[0];
-	}
+		correctAnswer: 0
+	},
 
-	var question4 = {
+	{
 		question: 'question1',
 		choices: ['answer1','answer2', 'answer3', 'answer4'],
-		correctAnswer: question1.choices[0];
-	}
+		correctAnswer: 0
+	},
 
-	var question5 = {
+	 {
 		question: 'question1',
 		choices: ['answer1','answer2', 'answer3', 'answer4'],
-		correctAnswer: question1.choices[0];
+		correctAnswer: 0
+	},
+
+	 {
+		question: 'question1',
+		choices: ['answer1','answer2', 'answer3', 'answer4'],
+		correctAnswer: 0
+	},
+
+	 {
+		question: 'question1',
+		choices: ['answer1','answer2', 'answer3', 'answer4'],
+		correctAnswer: 0
 	}
+]];
+
+var currentQuestionIndex = 0;
+//start button wire up ***************
+
+$('.start-button').click(function(){
+	//alert('this works!');
+	//hide landing page
+	$('.startPage').hide();
+	//show questions in order
+	$('.question-counter, .question-section').css('display', 'block');
+		
+	//display function that shows questions and choices.
+	// hints: .clone()
+	// yo
+	var clone = $('.inputList').clone();
+	$('.choices').append(clone)
+	var html = '';
+	// hints: html += "<p>some markup</p>"
+	LOOP
+		html += '<li><input class="user-option" type="radio" name="option" value="1"></li>'
+	$('.choices ul.inputList').append(html);
 });
+
+//display function
+
+// function questionDisplay(arrayToSearch, key){
+// 	//looping over an array of objects	
+// 	for (var i = 0; i < questionList.length; i++){
+// 		if(questionList[i][key] == arrayToSearch){
+// 			return i;
+// 		}
+
+// 	}
+//Hints: Flash out or empty content in an element
+// $(selector).empty();
+
+	//declare var for userInput
+
+
+
+
+//select question function
+$('.question-section').on('click', '.user-option', function() {
+	var userAnswer = $(this).val();
+ 
+ 	console.log(userAnswer);
+});
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
